dbfb75353fc6e2b5368112e228dda767b7681d9c
solden <solden@uw.edu>
2025-09-08 13:44:00 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Name from './components/Name.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <body>
+      <header className="navbar">
+        <div className="logo">SOLDEN STOLL</div>
+        <div className="nav-links">
+          <div className="menu">
+            <a href="#">Home</a>
+            <a href="#">Projects</a>
+            <a href="#">Experience</a>
+          </div>
+          <div className="socials">
+            <a href="#"><i className="fab fa-linkedin"></i></a>
+            <a href="#"><i className="fab fa-github"></i></a>
+            <a href="mailto:your@email.com"><i className="fas fa-envelope"></i></a>
+            <a href="#"><i className="fas fa-circle-nodes"></i></a>
+          </div>
+        </div>
       </header>
-    </div>
+    </body>
   );
 }
 
