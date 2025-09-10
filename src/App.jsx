e6388c0b065592d3b026fa29ed6be6da402c5267
solden <solden@uw.edu>
2025-09-09 23:59:41 -0700

    changed mobile styling

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <Header />
       <div className="fullpage-wrapper relative min-h-screen">
         <BackgroundImage path="personal-site-background.jpg"/>
-        <header className='fixed top-0 w-full h-[94.8px] bg-[var(--background-color)] border-b-2 border-[var(--grid-line-color)] z-40'></header>
+        <header className='fixed top-0 w-full h-[68px] md:h-[94.8px] bg-[var(--background-color)] border-b-2 border-[var(--grid-line-color)] z-40'></header>
         <div className="content-below">
           <div className="h-30 w-full hidden border-y border-y-(--pattern-fg) bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed [--pattern-fg:var(--color-black)]/5 md:block dark:[--pattern-fg:var(--color-white)]/10"></div>
           <div className='framed-box' id="about">
@@ -29,7 +29,7 @@ function App() {
               <p>© 2025 SOLDEN STOLL</p>
             </div>
           </footer>
-          <div className="h-30 w-full hidden border-y border-y-(--pattern-fg) bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed [--pattern-fg:var(--color-black)]/5 md:block dark:[--pattern-fg:var(--color-white)]/10"></div>
+          <div className="h-30 w-full block border-y border-y-(--pattern-fg) bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed [--pattern-fg:var(--color-black)]/5 md:block dark:[--pattern-fg:var(--color-white)]/10"></div>
         </div>
       </div>
     </div>
